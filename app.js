d7bb3468c2b3becb4fc76de3d5aaab9a5a3b9873
superman76 <cbloodjr@gmail.com>
2016-02-12 12:06:56 -0700

    4th Commit -retooled app.js

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-console.log("Hello World");
-
 var personOne = {firstName: "john", lastName: "johnson", phoneNumber: '404-333-3232'};
 var personTwo = {firstName: "sally", lastName: "johnson", phoneNumber: '404-334-2323'};
 var personThree = {firstName: "sue", lastName: "johnson", phoneNumber: '404-335-3223'};
 
 var people = [personOne, personTwo, personThree];
 
-var $list = $('#addPersonTable');
-addPersons.forEach(function(addPerson){
-	addRowToTable($list, addPerson);
-})
 
-function addaddPerson(event) {
-	var addPerson = {};
-	addPerson.firstName = $('#taskName').val();
-	addPerson.lastName = $('#taskName').val();
-	addPerson.phoneNumber = $('#dueDate').val();
 
-	if(addPerson.firstName && addPerson.lastName && addPerson.phoneNumber) {
-		var $list = $('#addPersonTable');
-		addRowToTable($list, addPerson);
-	}
-	console.log('Submitted with ', taskName, 'and', dueDate);
-	return false;
-}
 
-function addRowToTable($list, addPerson) {
-	$list.append('<tr><td>' + addPerson.firstName + '</td> \
-		<td>' + addPerson.lastName + '</td><td>' + addPerson.date + '</td> \
-		<td><button type="button"class="btn btn-primary deleteaddPerson">Delete</button></td></tr>');
-}
 
-function deleteaddPerson(event) {
-	var $button = $(event.target);
-	$button.closest('tr').remove();
 
-}
 
-$('#submit-button').on('click', addPerson);
-$('.deleteaddPerson').on('click', deletePerson);
+// 6) Loop through the array in the javascript file and spit the items 
+// out on to the page.
+
+// 7) Give the form functionality - when the user clicks submit - an 
+// alert pops up that contains the information in the fields. (BONUS: 
+// 	Append the content to the page)
