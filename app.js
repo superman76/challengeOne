edfb7f026c045b8fc33e087a85f22a46ea3038db
superman76 <cbloodjr@gmail.com>
2016-02-12 14:26:36 -0700

    6th commit

--- app.js
+++ app.js
@@ -4,14 +4,35 @@ var personThree = {firstName: "sue", lastName: "johnson", phoneNumber: '404-335-
 
 var people = [personOne, personTwo, personThree];
 
-person.forEach(function (personOne, personTwo, personThree) {
-	console.log(personOne, personTwo, personThree)
-});
+console.log(people);
+
+var addPersonToList = function(p)
+
+var $list = $('#personList');
+
+	var firstName = $('#firstName').val();
+	var lastName = $('#lastName').val();
+	var firstName = $('#firstName').val();
+
+	alert("First: " + firstName + "Last: " + lastName + "phoneNumber: ")
 
 
+	people.forEach(function(p){
+		$list.append(
+					'<tr> \
+					<td>' + p.firstName + '</td> \
+					<td>' + p.lastName + '</td> \
+					<td>' + p.phoneNumber + '</td> \
+					</tr>'
 
+			)
 
+		firstName = $('firstName').val('');
+		lastName = $('lastName').val('');
+		phoneNumber = $('phoneNumber').val('');
+	})
 
+	$('#submitButton').on('click', addPersonToList);
 
 // 6) Loop through the array in the javascript file and spit the items 
 // out on to the page.
@@ -20,9 +41,3 @@ person.forEach(function (personOne, personTwo, personThree) {
 // alert pops up that contains the information in the fields. (BONUS: 
 // 	Append the content to the page)
 // basic table, no buttons, try to get the data from the app.js to render
-
-// fruits.forEach(function (item, index, array) {
-//   console.log(item, index);
-// });
-// Apple 0
-// Banana 1
