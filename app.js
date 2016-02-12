4597b9b97936925ca83b611cbd2324995b33e918
superman76 <cbloodjr@gmail.com>
2016-02-12 11:22:17 -0700

    1st Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+var personOne = {firstName: "john", lastName: "johnson", phoneNumber: '404-333-3232'};
+var personTwo = {firstName: "sally", lastName: "johnson", phoneNumber: '404-334-2323'};
+var personThree = {firstName: "sue", lastName: "johnson", phoneNumber: '404-335-3223'};
+
+var people = [personOne, personTwo, personThree];
